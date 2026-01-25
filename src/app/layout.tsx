75ff3d85706b0f8d2a9cca7ee8d81ca7c4f4379c
merqtio <merqtio2@gmail.com>
2026-01-24 22:19:13 -0500

    Add Open Graph image for social embeds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,34 @@ export const metadata: Metadata = {
     ],
   },
   manifest: '/manifest.json',
+  
+  // Open Graph (Facebook, Discord, Slack, Telegram, etc.)
+  openGraph: {
+    title: 'Movement Core NFTs',
+    description: 'Community-driven selection of the top NFT collections on Movement blockchain',
+    url: 'https://movementnfts.com',
+    siteName: 'Movement Core NFTs',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Movement Core NFTs',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  
+  // Twitter Card
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Movement Core NFTs',
+    description: 'Community-driven selection of the top NFT collections on Movement blockchain',
+    images: ['/og-image.png'],
+  },
+  
+  metadataBase: new URL('https://movementnfts.com'),
 }
 
 export default function RootLayout({
