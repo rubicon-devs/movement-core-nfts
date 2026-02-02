0566f5510b2a741b0629ce4ec87cd59ee5377729
merqtio <merqtio2@gmail.com>
2026-02-02 10:09:13 -0500

    Increase winners grid to 5 columns on large desktop

--- src/components/ClientPage.tsx
+++ src/components/ClientPage.tsx
@@ -798,7 +798,7 @@ export function ClientPage() {
                 </div>
 
                 {winners.length > 0 ? (
-                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
                     {winners.map(winner => (
                       <CollectionCard
                         key={winner.id}
